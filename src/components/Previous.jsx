c1a18a7d5f55f6864894d4d48a53bd2d7d1eed60
Aman Islam <amanislam019@gmail.com>
2024-11-11 17:18:42 +0500

    Create Previous.jsx

---
+++ src/components/Previous.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { IoIosArrowBack } from "react-icons/io";
+
+const Previous = () => {
+  return (
+    <div className="flex items-center text-[#A3AED0] cursor-pointer border w-20 left-div mt-[50px] text-xs">
+      <IoIosArrowBack />
+      Back
+    </div>
+  );
+};
+
+export default Previous;

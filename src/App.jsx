d6e5679d3e3c44e0d249c398ece6aa1d2ebc1003
Aman Islam <amanislam019@gmail.com>
2024-12-09 16:45:19 +0500

    Some ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import SignIn from "./pages/Auth/SignIn";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Say from 'react-say';
 
 const App = () => {
   return(
     <>
+     <Say
+    pitch={ 1.1 }
+    rate={ 1.5 }
+    speak="A quick brown fox jumped over the lazy dogs."
+    volume={ .8 }
+  />
      <ToastContainer/>
      <SignIn />
     </>

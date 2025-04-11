de9e2dcfa43c5937c103d71d974a9668bf36ffa3
Aman Islam <amanislam019@gmail.com>
2025-04-11 23:48:19 +0500

    Update SignIn.jsx

--- src/pages/Auth/SignIn.jsx
+++ src/pages/Auth/SignIn.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import SignUpForm from "../../components/SignUpForm";
-import Footer from "../../components/footer";
-import Banner from "../../components/Banner"
+// import React from "react";
+// import SignUpForm from "../../components/SignUpForm";
+// import Footer from "../../components/footer";
+// import Banner from "../../components/Banner"
 
-const SignIn = () => {
-  return (
-    <div>
-    <div className="flex flex-col md:flex-row">
-      {/* Left div */}
-      <SignUpForm />
+// const SignIn = () => {
+//   return (
+//     <div>
+//     <div className="flex flex-col md:flex-row">
+//       {/* Left div */}
+//       <SignUpForm />
 
-      {/* Right div */}
-      <Banner/>
-    </div>
-    <Footer/>
-    </div>
-  );
-};
+//       {/* Right div */}
+//       <Banner/>
+//     </div>
+//     <Footer/>
+//     </div>
+//   );
+// };
 
-export default SignIn;
+// export default SignIn;

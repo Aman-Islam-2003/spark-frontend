011e4e927b770b831a4296c92731f0b544af9054
Aman Islam <amanislam019@gmail.com>
2025-04-11 23:48:21 +0500

    Update loginApi.js

--- src/services/loginApi.js
+++ src/services/loginApi.js
@@ -1,23 +1,23 @@
-import axios from "axios";
-import { toast } from "react-toastify";
+// import axios from "axios";
+// import { toast } from "react-toastify";
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL;
-console.log(backendUrl)
+// const backendUrl = import.meta.env.VITE_BACKEND_URL;
+// console.log(backendUrl)
 
-export const login = async (email, password) => {
-  try {
-    const response = await axios.post(`${backendUrl}/login`, {
-      email,
-      password,
-    });
-    if (response.status === 200) {
-      localStorage.setItem("token", response.data.token);
-      toast.success("Login successful!");
-    } else {
-      toast.error(response.data.message);
-    }
-  } catch (error) {
-    console.log(error);
-    toast.error("Login failed. Please try again.");
-  }
-};
+// export const login = async (email, password) => {
+//   try {
+//     const response = await axios.post(`${backendUrl}/login`, {
+//       email,
+//       password,
+//     });
+//     if (response.status === 200) {
+//       localStorage.setItem("token", response.data.token);
+//       toast.success("Login successful!");
+//     } else {
+//       toast.error(response.data.message);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     toast.error("Login failed. Please try again.");
+//   }
+// };

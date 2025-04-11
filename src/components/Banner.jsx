6df7288f224f906e17ac482b9d9bd156ad15afc1
Aman Islam <amanislam019@gmail.com>
2025-04-11 23:48:16 +0500

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import formImage from "../assets/form_image.png";
+// import React from 'react'
+// import formImage from "../assets/form_image.png";
 
-const Banner = () => {
-  return (
-    <div className="hidden w-full md:w-1/2 h-screen bg-[#E92928] text-white md:flex flex-col items-center justify-center rounded-bl-none md:rounded-bl-[150px]">
-        <div className="flex items-center justify-center w-2/3 md:w-2/5 gap-x-6 h-20 my-5">
-          <img src={formImage} className="w-[80px]" />
-          <div className="border-2 border-white rounded-lg py-2 px-10 md:px-16 text-lg md:text-2xl font-semibold">
-            Spark
-          </div>
-        </div>
+// const Banner = () => {
+//   return (
+//     <div className="hidden w-full md:w-1/2 h-screen bg-[#E92928] text-white md:flex flex-col items-center justify-center rounded-bl-none md:rounded-bl-[150px]">
+//         <div className="flex items-center justify-center w-2/3 md:w-2/5 gap-x-6 h-20 my-5">
+//           <img src={formImage} className="w-[80px]" />
+//           <div className="border-2 border-white rounded-lg py-2 px-10 md:px-16 text-lg md:text-2xl font-semibold">
+//             Spark
+//           </div>
+//         </div>
 
-        <div className="border rounded-2xl text-center mt-6 px-6 md:px-14 py-6 font-montserrat logo">
-          <div className="text-sm">Learn more about airdrive on</div>
-          <div className="font-semibold text-xl">Spark.pl</div>
-        </div>
-      </div>
-  )
-}
+//         <div className="border rounded-2xl text-center mt-6 px-6 md:px-14 py-6 font-montserrat logo">
+//           <div className="text-sm">Learn more about airdrive on</div>
+//           <div className="font-semibold text-xl">Spark.pl</div>
+//         </div>
+//       </div>
+//   )
+// }
 
-export default Banner
+// export default Banner
